1d980c92fc5f0f07964cb67f189b6372abd3a587
Mostafa Magdy <49917088+Engineer-mostafa@users.noreply.github.com>
2020-09-20 18:59:47 +0200

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyAppState extends State<MyApp> {
   // ignore: non_constant_identifier_names
   var _q_number = 0;
   // ignore: non_constant_identifier_names
-  var _total_s = 0;
+  double _total_s = 0;
 
 
   // ignore: unused_element
@@ -28,7 +28,7 @@ class MyAppState extends State<MyApp> {
     });
   }
   // ignore: non_constant_identifier_names
-  void _increase_q_Number(int score) {
+  void _increase_q_Number(double score) {
     _total_s += score;
     setState(() {
       _q_number += 1;
@@ -41,38 +41,39 @@ class MyAppState extends State<MyApp> {
       {
         'questionText': '105 * 5 = ?',
         'answers': [
-          {'text': '525', 'score': 10},
-          {'text': '315', 'score': 0},
-          {'text': '225', 'score': 0},
-          {'text': '645', 'score': 0},
+          {'text': '525', 'score': 100.0/3},
+          {'text': '315', 'score': 0.0},
+          {'text': '225', 'score': 0.0},
+          {'text': '645', 'score': 0.0},
         ],
       },
       {
         'questionText': '550 / 5 = ?',
         'answers': [
-          {'text': '11', 'score': 0},
-          {'text': '110', 'score': 10},
-          {'text': '120', 'score': 0},
-          {'text': '12', 'score': 0},
+          {'text': '11', 'score': 0.0},
+          {'text': '110', 'score': 100.0/3},
+          {'text': '120', 'score': 0.0},
+          {'text': '12', 'score': 0.0},
         ],
       },
       {
         'questionText': '5 choose 5 = ?',
         'answers': [
-          {'text': '5', 'score': 0},
-          {'text': '1', 'score': 10},
-          {'text': '0', 'score': 0},
-          {'text': '10', 'score': 0}
+          {'text': '5', 'score': 0.0},
+          {'text': '1', 'score': 100.0/3},
+          {'text': '0', 'score': 0.0},
+          {'text': '10', 'score': 0.0}
         ],
       },
     ];
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('Basics Math Quiz'),
         ),
-        body: _q_number <= 2
+        body: _q_number <= 2 
             ? Quiz(
                 questions: _questions,
                 answerQuestion: _increase_q_Number,
--- lib/results.dart
+++ lib/results.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Results extends StatelessWidget {
-  final int score;
+  final double score;
   final Function reset;
 
   String get resultPhrase {
     String resultText;
-    if (score > 20) {
-      resultText = 'You are awesome and innocent! your result is $score';
-    } else if (score > 10) {
-      resultText = 'Pretty likeable! your result is $score';
-    } else if (score > 0) {
-      resultText = 'You are ... strange?! your result is $score';
+    if (score == 100) {
+      resultText =
+          'You are awesome and innocent! your result is ${score.toStringAsFixed(2)}%';
+    } else if (score > 50) {
+      resultText = 'Pretty likeable! your result is ${score.toStringAsFixed(2)}%';
+    } else if (score > 20) {
+      resultText = 'You are ... strange?! your result is ${score.toStringAsFixed(2)}%';
     } else {
-      resultText = 'You are so bad! your result is $score';
+      resultText = 'You are so bad! your result is ${score.toStringAsFixed(2)}%';
     }
     return resultText;
   }
 
-
-  Results(this.score,this.reset);
+  Results(this.score, this.reset);
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        
         children: <Widget>[
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           Text(
             resultPhrase,
             style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
